b0a5cc01c901c73acf68dc4f5cc10a91ef33fb62
gitclass10am <gitclass10am@gmail.com>
2017-12-02 11:07:28 +0530

    all files

---
+++ sampleprogram.java
@@ -0,0 +1,17 @@
+class sampleprogram
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a=40;
+b=70;
+c=10;
+if(a>b && a>c)
+System.out.println("A is Big value of Three values "+a);
+else
+if(b>a && b>c)
+System.out.println("A is Big value of Three values "+a)[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[F[B[F[B[F[B[F[B[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+else[A[F[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[3~[3~[b[B[F[B[F[B[C[C[C[C
+System.out.println("C is Big Vlaue of Three Values "+c);
+}
+}
